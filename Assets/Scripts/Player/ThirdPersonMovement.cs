1da48584879c47dd983fa4617553b5f7eb056a05
Kemal <kemal_zahid@hotmail.com>
2022-11-22 20:31:22 +0300

    Player Movement

---
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float Speed = 6f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
