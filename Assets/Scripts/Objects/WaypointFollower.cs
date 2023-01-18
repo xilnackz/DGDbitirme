33c0d813f1b19eff645c8752ec1edd0e27ce1686
cesusa <61979990+cesusa@users.noreply.github.com>
2023-01-18 23:14:30 +0300

    s

--- Assets/Scripts/Objects/WaypointFollower.cs
+++ Assets/Scripts/Objects/WaypointFollower.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
+    [SerializeField] GameObject[] waypoints;
+    int currentWayPointIndex = 0;
+
+    [SerializeField] float speed = 1f;
+
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,16 @@ public class WaypointFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, waypoints[currentWayPointIndex].transform.position) < .1f)
+        {
+
+            currentWayPointIndex++;
+            if(currentWayPointIndex >= waypoints.Length)
+            {
+                currentWayPointIndex = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWayPointIndex].position, speed * Time.deltaTime);
     }
 }
