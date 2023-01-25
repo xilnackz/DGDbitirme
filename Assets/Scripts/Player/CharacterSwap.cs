b2cab8ede40930cab613a8f7583042bff979ed10
xilnackz <73229241+xilnackz@users.noreply.github.com>
2023-01-25 22:09:55 +0300

    Dura Cross eklendi

--- Assets/Scripts/Player/CharacterSwap.cs
+++ Assets/Scripts/Player/CharacterSwap.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CharacterSwap : MonoBehaviour
 {
-    public GameObject temerario, dura;
+    public GameObject temerario, dura, cross;
     public int whichcharon = 1;
     void Start()
     {
         temerario.gameObject.SetActive(true);
         dura.gameObject.SetActive(false);
+        cross.gameObject.SetActive(false);
+        
     }
 
     public void SwapCharacter()
@@ -21,12 +23,14 @@ public class CharacterSwap : MonoBehaviour
                 whichcharon = 2;
                 temerario.gameObject.SetActive(false);
                 dura.gameObject.SetActive(true);
+                cross.gameObject.SetActive(true);
                 break;
             
             case 2:
                 whichcharon = 1;
                 temerario.gameObject.SetActive(true);
                 dura.gameObject.SetActive(false);
+                cross.gameObject.SetActive(false);
                 break;
         }
     }
